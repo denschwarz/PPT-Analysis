50547b46cc35c5321eb85f76f31ecc5cf5a0e03d
Dennis Schwarz <dennis.schwarz@desy.de>
2019-03-23 14:14:04 +0100

    test this version as starting point

--- src/Analysis.cc
+++ src/Analysis.cc
@@ -27,7 +27,6 @@ int main(int argc, char* argv[]){
   vector<TH1F*> hist_muonCHARGE = CreateHistograms("muonCHARGE", 5, -2.5, 2.5);
   vector<TH1F*> hist_elecPT = CreateHistograms("elecPT", 20, 0, 200);
   vector<TH1F*> hist_Zmass = CreateHistograms("Zmass", 20, 0, 150);
-  vector<TH1F*> hist_HIGGSmass = CreateHistograms("HIGGSmass", 40, 0, 200);
 
   // ---------------------------------------------------------------------------
   // Hier findet die eigentliche Analyse statt.
@@ -92,116 +91,10 @@ int main(int argc, char* argv[]){
           In dem 2mu2el Kanal ist hier die Zuordnung eindeutig, weil ein Z
           immer in 2 Myonen oder 2 Elektronen zerfällt.
           */
-          Particle Z1 = Combine(Muons[0], Muons[1]);
-          Particle Z2 = Combine(Elecs[0], Elecs[1]);
+          Particle Z1;
+          Particle Z2;
           FillHistogram(hist_Zmass, Z1.Mass(), weight, process);
           FillHistogram(hist_Zmass, Z2.Mass(), weight, process);
-
-          // Hier ist das Higgs
-          Particle H = Combine(Z1, Z2);
-          FillHistogram(hist_HIGGSmass, H.Mass(), weight, process);
-        }
-        else if(channel == "4mu"){
-          // Finde mögliche Kombinationen für Muon[0] mit Hilfe der Ladung
-          // Das zweite Z sind dann die verbleibenden 2 Myonen
-          Particle ZA1, ZA2, ZB1, ZB2;
-          int charge0 = Muons[0].Charge();
-          int charge1 = Muons[1].Charge();
-          int charge2 = Muons[2].Charge();
-          int charge3 = Muons[3].Charge();
-          if(charge0 != charge1 && charge0 != charge2){
-            ZA1 = Combine(Muons[0], Muons[1]);
-            ZA2 = Combine(Muons[2], Muons[3]);
-            ZB1 = Combine(Muons[0], Muons[2]);
-            ZB2 = Combine(Muons[1], Muons[3]);
-          }
-          if(charge0 != charge1 && charge0 != charge3){
-            ZA1 = Combine(Muons[0], Muons[1]);
-            ZA2 = Combine(Muons[2], Muons[3]);
-            ZB1 = Combine(Muons[0], Muons[3]);
-            ZB2 = Combine(Muons[1], Muons[2]);
-          }
-          if(charge0 != charge2 && charge0 != charge3){
-            ZA1 = Combine(Muons[0], Muons[2]);
-            ZA2 = Combine(Muons[1], Muons[3]);
-            ZB1 = Combine(Muons[0], Muons[3]);
-            ZB2 = Combine(Muons[1], Muons[2]);
-          }
-          // Jetzt muss noch Kombination A oder B ausgewählt werden.
-          // Prüfe dafür welches der 4 Z dichter an der Z-Masse von 91 GeV liegt
-          Particle Z1, Z2;
-          double deltaZA1 = fabs(ZA1.Mass() - 91);
-          double deltaZA2 = fabs(ZA2.Mass() - 91);
-          double deltaZB1 = fabs(ZB1.Mass() - 91);
-          double deltaZB2 = fabs(ZB2.Mass() - 91);
-          if(deltaZA1 < deltaZB1 && deltaZA1 < deltaZB2){
-            Z1 = ZA1;
-            Z2 = ZA2;
-          }
-          else if(deltaZA2 < deltaZB1 && deltaZA2 < deltaZB2){
-            Z1 = ZA1;
-            Z2 = ZA2;
-          }
-          else{
-            Z1 = ZB1;
-            Z2 = ZB2;
-          }
-          FillHistogram(hist_Zmass, Z1.Mass(), weight, process);
-          FillHistogram(hist_Zmass, Z2.Mass(), weight, process);
-
-          Particle H = Combine(Z1, Z2);
-          FillHistogram(hist_HIGGSmass, H.Mass(), weight, process);
-        }
-        else if(channel == "4el"){
-          // Finde mögliche Kombinationen für Muon[0] mit Hilfe der Ladung
-          // Das zweite Z sind dann die verbleibenden 2 Myonen
-          Particle ZA1, ZA2, ZB1, ZB2;
-          int charge0 = Elecs[0].Charge();
-          int charge1 = Elecs[1].Charge();
-          int charge2 = Elecs[2].Charge();
-          int charge3 = Elecs[3].Charge();
-          if(charge0 != charge1 && charge0 != charge2){
-            ZA1 = Combine(Elecs[0], Elecs[1]);
-            ZA2 = Combine(Elecs[2], Elecs[3]);
-            ZB1 = Combine(Elecs[0], Elecs[2]);
-            ZB2 = Combine(Elecs[1], Elecs[3]);
-          }
-          if(charge0 != charge1 && charge0 != charge3){
-            ZA1 = Combine(Elecs[0], Elecs[1]);
-            ZA2 = Combine(Elecs[2], Elecs[3]);
-            ZB1 = Combine(Elecs[0], Elecs[3]);
-            ZB2 = Combine(Elecs[1], Elecs[2]);
-          }
-          if(charge0 != charge2 && charge0 != charge3){
-            ZA1 = Combine(Elecs[0], Elecs[2]);
-            ZA2 = Combine(Elecs[1], Elecs[3]);
-            ZB1 = Combine(Elecs[0], Elecs[3]);
-            ZB2 = Combine(Elecs[1], Elecs[2]);
-          }
-          // Jetzt muss noch Kombination A oder B ausgewählt werden.
-          // Prüfe dafür welches der 4 Z dichter an der Z-Masse von 91 GeV liegt
-          Particle Z1, Z2;
-          double deltaZA1 = fabs(ZA1.Mass() - 91);
-          double deltaZA2 = fabs(ZA2.Mass() - 91);
-          double deltaZB1 = fabs(ZB1.Mass() - 91);
-          double deltaZB2 = fabs(ZB2.Mass() - 91);
-          if(deltaZA1 < deltaZB1 && deltaZA1 < deltaZB2){
-            Z1 = ZA1;
-            Z2 = ZA2;
-          }
-          else if(deltaZA2 < deltaZB1 && deltaZA2 < deltaZB2){
-            Z1 = ZA1;
-            Z2 = ZA2;
-          }
-          else{
-            Z1 = ZB1;
-            Z2 = ZB2;
-          }
-          FillHistogram(hist_Zmass, Z1.Mass(), weight, process);
-          FillHistogram(hist_Zmass, Z2.Mass(), weight, process);
-
-          Particle H = Combine(Z1, Z2);
-          FillHistogram(hist_HIGGSmass, H.Mass(), weight, process);
         }
 
         // Anzahl an Elektronen bzw. Myonen
@@ -220,11 +113,6 @@ int main(int argc, char* argv[]){
           FillHistogram(hist_muonETA, eta, weight, process);
           FillHistogram(hist_muonPHI, phi, weight, process);
           FillHistogram(hist_muonCHARGE, charge, weight, process);
-
-          // ptmax finden
-          if(process == "data"){
-            if(pt > ptmax) ptmax = pt;
-          }
         }
         // ---------------------------------------------------------------------
 
@@ -235,20 +123,11 @@ int main(int argc, char* argv[]){
           double pt = Elecs[i].Pt();
           // in Histogramme füllen
           FillHistogram(hist_elecPT, pt, weight, process);
-          // ptmax finden
-          if(process == "data"){
-            if(pt > ptmax) ptmax = pt;
-          }
         }
         // ---------------------------------------------------------------------
       }
     }
   }
-  cout << "Events in total: " << events_tot << endl;
-  cout << "Events in 2mu2el: " << events_2mu2el << endl;
-  cout << "Events in 4mu: " << events_4mu << endl;
-  cout << "Events in 4el: " << events_4el << endl;
-  cout << "max pT in data: " << ptmax << endl;
 
   // Am Ende: Abspeichern der Histogramme in der Output Datei
   outputFile->Write();
